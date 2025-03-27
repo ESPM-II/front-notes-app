3cd22147da48ed5ffa09fe87899d9feadcc972dd
ESPM-II <jjaolivos.02@gmail.com>
2025-03-27 15:42:04 -0300

    feat: Implementar ThemeProvider para solucionar problemas de hidratación en Next con Material UI

---
+++ src/providers/ThemeProvider.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { ReactNode, useEffect, useState } from "react";
+
+const theme = createTheme({
+    palette: {
+        mode: "light"
+    },
+});
+
+export default function ThemeRegistry({ children } : { children: ReactNode}) {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, [])
+
+    if(!mounted) {
+        return null;
+    }
+
+    return(
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    );
+}
